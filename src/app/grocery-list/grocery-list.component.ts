8c92de5dc9850c3a23620036b351a5365735aa35
Robert <robjkc@yahoo,com>
2019-08-22 22:52:18 -0500

    add grocery

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-grocery-list',
-  templateUrl: './grocery-list.component.html',
-  styleUrls: ['./grocery-list.component.css']
+    selector: 'app-grocery-list',
+    templateUrl: './grocery-list.component.html',
+    styleUrls: ['./grocery-list.component.css']
 })
 export class GroceryListComponent implements OnInit {
 
-  constructor() { }
+    list: any;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.list = [
+            { name: 'Milk', price: 3 },
+            { name: 'Cereal', price: 4 },
+            { name: 'OJ', price: 2.50 },
+        ];
+    }
 
 }
